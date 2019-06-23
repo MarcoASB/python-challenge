77f480b7efb708e77e60bcef5e95c8483d6d6777
MarcoASB <50678409+MarcoASB@users.noreply.github.com>
2019-06-23 00:11:53 -0500

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,10 +6,13 @@ path_election_data_file_csv = os.path.join('Election.csv')
 
 def profile_per_candidate(dataset, total_votes):
     lcandidate = {'stats':{}}
+    #A complete list of candidates who received votes
     for candidate in dataset:    
         if candidate[2] not in lcandidate.items():
             lcandidate['stats'][candidate[2]] = 0
-
+    
+    #The total number of votes each candidate won
+    #The percentage of votes each candidate won
     votes_number = 0
     for name in lcandidate['stats']:
         for rows in dataset:
@@ -20,6 +23,7 @@ def profile_per_candidate(dataset, total_votes):
                 lcandidate['stats'][name].append(0)
         votes_number = 0
     
+    #The winner of the election based on popular vote.
     winner = 0
     for c in lcandidate['stats']:
         if lcandidate['stats'][c][0] > winner:
@@ -27,9 +31,9 @@ def profile_per_candidate(dataset, total_votes):
     return lcandidate
 
 def poll_results():
-    #Dict where all data will be allocated
     election_results = {
-            'headers': ['  ELECTION RESULTS  ', '-------------------------'],
+            'headers':'  ELECTION RESULTS  ',
+            'separate': " ---------------------- ",
             'votes': 0,
             'labels': ['Total Votes', 'Winner'],
             'candidates': {}
@@ -38,23 +42,19 @@ def poll_results():
     with open(path_election_data_file_csv, 'r') as election_data_file:
         election_data_ds = csv.reader(election_data_file, delimiter=',')
         next(election_data_ds)
-        
-        #Comprehension list of elections.
         election_data_list = [election for election in election_data_ds]
-        
-        #Get total votes, no matter the candidate.
+        #The total number of votes cast
         election_results['votes'] = len(election_data_list)
-        #Get profile for each candidate
         election_results['candidates'] = profile_per_candidate(election_data_list, election_results['votes'])
         
-        #Print out Election Results
+        #Print out Results
         print("")
-        print(f"   {election_results['headers'][0]}\n{election_results['headers'][1]}")
+        print(f"   {election_results['headers']}")
         print(f"{election_results['labels'][0]}: {election_results['votes']}")
-        print(f"{election_results['headers'][1]}")
+        print(f"{election_results['separate']}")
         for candidate in election_results['candidates']['stats']:
             print(f"{candidate}: {election_results['candidates']['stats'][candidate][1]:.3f}% ({election_results['candidates']['stats'][candidate][0]})")
-        print(f"{election_results['headers'][1]}")
+        print(f"{election_results['separate']}")
         for winner in election_results['candidates']['stats']:
             if int(election_results['candidates']['stats'][winner][2]) == 1:
                 print(f"           {election_results['labels'][1]}: {winner}  ")
